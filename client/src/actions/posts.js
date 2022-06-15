4562a29785cd4d341c192db7945c80e4b34e8750
Sadick Abubakar Yahaya <abubakar.yahaya@amalitech.org>
2022-06-15 08:51:53 +0000

    reducers and actions linkd

---
+++ client/src/actions/posts.js
@@ -0,0 +1,17 @@
+import * as api from '../api';
+
+//action creators
+
+const getPosts = () => async (dispatch) => {
+    try {
+        const {data} = await api.fetchPosts();
+
+        dispatch({ type: 'FETCH_ALL', payload: data });
+    }
+    catch (error) {
+        console.log(error.message);
+    }
+    
+}
+
+export default getPosts;
